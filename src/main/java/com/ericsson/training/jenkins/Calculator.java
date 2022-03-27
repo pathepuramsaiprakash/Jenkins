b7aa1e47dd9dc0af4202c630415aff98b09e82da
Vamshi <saiprakash.vip@gmail.com>
2022-03-27 15:51:35 +0530

    first

---
+++ src/main/java/com/ericsson/training/jenkins/Calculator.java
@@ -0,0 +1,12 @@
+package com.ericsson.training.jenkins;
+
+public class Calculator {
+	public int add_num(int x,int y) {
+		return x+y;
+	}
+	public int sub_num(int x,int y)
+	{
+		return x-y;
+	}
+
+}
---
+++ src/test/java/com/ericsson/training/jenkins/Test_calc.java
@@ -0,0 +1,13 @@
+package com.ericsson.training.jenkins;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Test_calc extends Calculator {
+
+	@Test
+	public void add_test() {
+		assertEquals(14,this.add_num(9,5));
+	}
+
+}
